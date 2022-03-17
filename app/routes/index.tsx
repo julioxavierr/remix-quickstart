d6a6f32ec7d71bb175faab74eda511a7cd074bbe
Júlio Xavier <julioxavierr@gmail.com>
2022-03-17 19:08:34 -0300

    setup tailwind

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -1,10 +1,17 @@
-import { Link } from 'remix';
+import { Link } from "remix";
 
 export default function Index() {
   return (
     <div style={{ fontFamily: "system-ui, sans-serif", lineHeight: "1.4" }}>
-      <h1>Welcome to Remix</h1>
-      <Link to="/posts">Posts</Link>
+      <h1 className="text-3xl font-bold underline">Welcome to Remix</h1>
+      <ul>
+        <li>
+          <Link to="/posts">Posts</Link>
+        </li>
+        <li>
+          <Link to="/admin">Admin</Link>
+        </li>
+      </ul>
     </div>
   );
 }
